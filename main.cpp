3c654f02674b1174a9bf2e77dd632a4b69aa1fe2
Dmitry Lichmanov <dimlichmanov@gmail.com>
2020-12-11 16:24:02 +0300

    final version of 1 task

--- main.cpp
+++ main.cpp
@@ -25,27 +25,36 @@ int main(int argc, char** argv) {
 
     if (rank == 0) {
         /*Generate values*/
-        int storage[10000];
-        int displaces[size];
+        int storage[10000];   //a storage of messages in the buffer of (0,0) process
+        int displaces[size];  //array of displaces
         int current_displace = 0;
         displaces[0] = 0;
+
         for (int i = 1; i < 16; i++){
-            displaces[i] = std::rand() % 100 + current_displace;
-            current_displace += displaces[i] + i;
+            int gap_size = std::rand() % 100;
+            displaces[i] = current_displace + gap_size;
+            current_displace += i;
         }
+
         for (int i = 1; i < 16; i++) {
-            for (int j = 1; j < i; j++) {
-                storage[displaces[i] + j] = std::rand() % 100;
+            for (int j = 0; j < i; j++) {
+//                storage[displaces[i] + j] = std::rand() % 100;
+                storage[displaces[i] + j] = i;
             }
         }
 
         /*Send to processes*/
         for (int i = 1; i < 16; i++) {
-            MPI_Send(storage + displaces[i], i, MPI_INT, i, MPI_ANY_TAG, TRANSPUTER_MATRIX);
+            MPI_Send(storage + displaces[i], i, MPI_INT, i, 0, TRANSPUTER_MATRIX);
         }
 
     } else {
         int storage[rank];
         MPI_Recv(storage, rank, MPI_INT, 0, MPI_ANY_TAG, TRANSPUTER_MATRIX, MPI_STATUS_IGNORE);
+        for (int i = 0; i < rank; i++) {
+            printf("Process num %d has element %d\n", rank, storage[i]);
+            //std::cout<<"process num " << rank <<" has element " <<storage[i] <<std::endl;
+        }
     }
+    MPI_Barrier(TRANSPUTER_MATRIX);
 }
