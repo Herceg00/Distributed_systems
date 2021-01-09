af3967c3efb3898cdc1f87ed3a42e5d94320d188
Dmitry Lichmanov <dimlichmanov@gmail.com>
2021-01-10 02:12:49 +0300

    Raw version of 1 task

--- task1.cpp
+++ task1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "mpich/mpi.h"
+#include <vector>
+#include <utility>
+#include <array>
 
 
 int main(int argc, char** argv) {
@@ -12,6 +15,43 @@ int main(int argc, char** argv) {
     int coord[2];
     int rank;
     int size;
+    int total_size = 120;
+
+    // std:: pair ( ELEMENTS TO THE RIGHT | ELEMENTS TO THE DOWN ) - according to proposed strategy
+    std::array<std::pair<int, int>, 15> routing = {std::pair<int, int>(49,71),
+                                               std::pair<int, int>(29,19),
+                                               std::pair<int, int>(12,15),
+                                               std::pair<int, int>(0,9),
+                                               std::pair<int, int>(18,49),
+                                               std::pair<int, int>(14,18),
+                                               std::pair<int, int>(8,15),
+                                               std::pair<int, int>(0,10),
+                                               std::pair<int, int>(18,23),
+                                               std::pair<int, int>(15,12),
+                                               std::pair<int, int>(9,11),
+                                               std::pair<int, int>(0,8),
+                                               std::pair<int, int>(11,0),
+                                               std::pair<int, int>(10,0),
+                                               std::pair<int, int>(7,0)};
+
+    std::array<std::pair<int, int>, 15> providers = {std::pair<int, int>(0,-1),
+                                                   std::pair<int, int>(1,-1),
+                                                   std::pair<int, int>(2,-1),
+                                                   std::pair<int, int>(-1,0),
+                                                   std::pair<int, int>(4,1),
+                                                   std::pair<int, int>(5,2),
+                                                   std::pair<int, int>(6,3),
+                                                   std::pair<int, int>(-1,4),
+                                                   std::pair<int, int>(8,5),
+                                                   std::pair<int, int>(9,6),
+                                                   std::pair<int, int>(10,7),
+                                                   std::pair<int, int>(-1,8),
+                                                   std::pair<int, int>(12,9),
+                                                   std::pair<int, int>(13,10),
+                                                   std::pair<int, int>(14,11)};
+
+
+
 
     /*Create a cartesian grid*/
     MPI_Comm TRANSPUTER_MATRIX;
@@ -23,37 +63,45 @@ int main(int argc, char** argv) {
     fflush(stdout);
     MPI_Barrier(TRANSPUTER_MATRIX);
 
-    if (rank == 0) {
-        /*Generate values*/
-        int storage[10000];   //a storage of messages in the buffer of (0,0) process
-        int displaces[size];  //array of displaces
-        int current_displace = 0;
-        displaces[0] = 0;
+    int buffer_size = rank + routing[rank].first + routing[rank].second;
+    int *buffer = new int[buffer_size];
 
-        for (int i = 1; i < 16; i++){
-            int gap_size = std::rand() % 100;
-            displaces[i] = current_displace + gap_size;
-            current_displace += i;
-        }
+    MPI_Barrier(TRANSPUTER_MATRIX);
 
-        for (int i = 1; i < 16; i++) {
-            for (int j = 0; j < i; j++) {
-                storage[displaces[i] + j] = std::rand() % 100;
-            }
-        }
 
-        /*Send to processes*/
-        for (int i = 1; i < 16; i++) {
-            MPI_Send(storage + displaces[i], i, MPI_INT, i, 0, TRANSPUTER_MATRIX);
-        }
+    //STEP 1
 
-    } else {
-        int storage[rank];
-        MPI_Recv(storage, rank, MPI_INT, 0, MPI_ANY_TAG, TRANSPUTER_MATRIX, MPI_STATUS_IGNORE);
-        for (int i = 0; i < rank; i++) {
-            printf("Process num %d has element %d\n", rank, storage[i]);
-            //std::cout<<"process num " << rank <<" has element " <<storage[i] <<std::endl;
-        }
+    if (rank == 0) { //do parallel send
+        MPI_Send(buffer, routing[rank].first, MPI_INT, 1, 0, TRANSPUTER_MATRIX);
+        MPI_Send(buffer, routing[rank].second, MPI_INT, 4, 0, TRANSPUTER_MATRIX);
     }
+    if (rank == 1 or rank == 4) {
+        MPI_Recv(buffer,buffer_size, MPI_INT, 0, MPI_ANY_TAG,TRANSPUTER_MATRIX, nullptr);
+    }
+
+    //STEP 2
+
+    if (rank == 1 or rank == 4) { //do parallel send
+        MPI_Send(buffer, total_size, MPI_INT, 1, 0, TRANSPUTER_MATRIX);
+        MPI_Send(buffer, total_size, MPI_INT, 4, 0, TRANSPUTER_MATRIX);
+    }
+    if (rank == 2 or rank == 8) {
+        MPI_Recv(buffer,buffer_size, MPI_INT, 0, MPI_ANY_TAG,TRANSPUTER_MATRIX, nullptr);
+    }
+
+    if (rank == 5) {
+        MPI_Recv(buffer,buffer_size, MPI_INT, 0, MPI_ANY_TAG,TRANSPUTER_MATRIX, nullptr);
+    }
+
+    //STEP 3
+
+    //STEP 4
+
+    //STEP 5
+
+    //STEP 6
+
+
+
     MPI_Barrier(TRANSPUTER_MATRIX);
 }
